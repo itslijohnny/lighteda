c1f2510694bf67708fccfe4f7d7157f47a35977b
j84lee <j84lee@users.noreply.github.com>
2019-02-19 15:48:58 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
-     name='LightEDA',
+     name='lighteda',
      version='0.0.1',
      # scripts=['dokr'],
      author="Johnny Li",
